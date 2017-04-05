569c4101b0878f300cecc3a6806bf4f32e7533dc
Baptiste Perrin <perrin.baptiste@yahoo.fr>
2017-04-05 08:45:59 +0200

    corrected infos for push on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,15 +61,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'sample-app-b-perrin.herokuapp.com'
+  host = 'https://sample-app-b-perrin.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => app65526477@heroku.com,
-    :password       => bdelivpr0201,
-    :domain         => 'heroku.com',
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'herokuapp.com',
     :enable_starttls_auto => true
   }
 
